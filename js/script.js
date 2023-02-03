509b8352b3c704070449d4b8002356321929bcf9
Vladyslav <95444534+GreenVladyslav@users.noreply.github.com>
2023-02-03 15:28:50 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const btn = document.querySelector('.btn'),
+      body = document.body,
+      toggleText = document.querySelector('.toggleMode')
+      link = document.querySelectorAll('a'),
+
+btn.addEventListener('click', () => {
+
+    body.classList.toggle('dark-mode');
+
+    if (body.classList.contains('dark-mode')) {
+        toggleText.textContent = 'Change to Light Mode';
+        
+        link.forEach(a => {
+            a.style.color = '#4e5b6f';
+            a.style.textDecoration = 'underline'
+        })
+    } else {
+        toggleText.textContent = 'Change to Dark Mode';
+        link.forEach(a => {
+            a.style.color = '#5a513666';
+            a.style.textDecoration = 'underline'
+        })
+    }
+    
+});
+
